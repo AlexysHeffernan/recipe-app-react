10f04ea77e61a248a3429ccabe4a762badf9c45d
Alexys Heffernan <alexysheffernan@gmail.com>
2024-05-27 17:39:15 -0400

    added test

--- src/RecipeCreate.js
+++ src/RecipeCreate.js
@@ -34,16 +34,7 @@ function RecipeCreate({createRecipe}) {
   return (
     <form name="create" onSubmit={handleSubmit}>
       <table >
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Cuisine</th>
-          <th>Photo</th>
-          <th>Ingredients</th>
-          <th>Preparation</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
+  
         <tbody>
           <tr>
             <td>
